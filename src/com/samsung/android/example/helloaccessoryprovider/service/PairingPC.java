9b528b6f9733c65594153b4bbd2deaa6f46cacd4
Quki <quki09@naver.com>
2015-07-18 17:19:53 +0900

    FIRST COMMIT

---
+++ src/com/samsung/android/example/helloaccessoryprovider/service/PairingPC.java
@@ -0,0 +1,22 @@
+package com.samsung.android.example.helloaccessoryprovider.service;
+
+import com.samsung.android.example.helloaccessoryprovider.R;
+import com.samsung.android.example.helloaccessoryprovider.R.id;
+import com.samsung.android.example.helloaccessoryprovider.R.layout;
+import com.samsung.android.example.helloaccessoryprovider.R.menu;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class PairingPC extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_pairing_pc);
+	}
+
+
+}
